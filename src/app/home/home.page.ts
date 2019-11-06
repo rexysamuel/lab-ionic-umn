caa3cb2512fc4f7da292e0357673dad35769e409
Rexy Samuel <reximux@gmail.com>
2019-11-06 09:24:29 +0700

    Weel-11

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,22 +1,19 @@
-import { TodoService } from './../services/todo.service';
-import { Component, OnInit } from '@angular/core';
-import { Todo } from '../services/todo.service';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
-  todos: Todo[];
-  constructor(private todoService: TodoService) {}
+export class HomePage {
 
-  ngOnInit(){
-    this.todoService.getTodos().subscribe(res => {
-      this.todos = res;
-    });
+  constructor(private router: Router, private authSvc: AuthService) { }
+
+  onLogout() {
+    this.authSvc.logout();
+    this.router.navigateByUrl('/auth');
   }
-  remove(item){
-    this.todoService.removeTodo(item.id);
-  }
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyDr4QM4HOnopKVRGm7jRFnik14vGsAAkrI",
-    authDomain: "umn-ionic-backend-63cd6.firebaseapp.com",
-    databaseURL: "https://umn-ionic-backend-63cd6.firebaseio.com",
-    projectId: "umn-ionic-backend-63cd6",
-    storageBucket: "umn-ionic-backend-63cd6.appspot.com",
-    messagingSenderId: "553610851719",
-    appId: "1:553610851719:web:ec6eb3ade059e3ba950f22",
-    measurementId: "G-GSK3S4H97F"
-  }
+  firebaseAPIKey: 'AIzaSyDr4QM4HOnopKVRGm7jRFnik14vGsAAkrI'
 };
 
 /*
